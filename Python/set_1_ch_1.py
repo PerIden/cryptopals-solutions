1323e4505e5af62bb9254eb84c5076b9522952c1
PerIden <per.idenfeldt@hotmail.com>
2020-10-12 20:05:37 +0200

    first challenge

---
+++ Python/set_1_ch_1.py
@@ -0,0 +1,21 @@
+'''
+Convert hex to base64
+
+The string:
+
+49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+
+Should produce:
+
+SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+So go ahead and make that happen. You'll need to use this code for the rest of the exercises. 
+'''
+import codecs
+
+def hex_to_base64(hex):
+    base64 = codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
+    return base64
+
+#hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+#print(hex_to_base64(hex))
